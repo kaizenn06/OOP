54e6b6ad76d9a76ea8bcaa5384267fe5f292f2e4
Kaizen <haibui1362006hd@gmail.com>
2026-08-10 15:20:47 +0700

    Create J01004 — SỐ NGUYÊN TỐ.java

---
+++ LẬP TRÌNH JAVA CƠ BẢN/J01004 — SỐ NGUYÊN TỐ.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class b24dccn186 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int ok = 1;
+            for (int i = 2 ; i * i <= n ; i++) {
+                if (n % i == 0) {
+                    ok = 0;
+                    break;
+                }
+            }
+            System.out.println(ok == 1 ? "YES" : "NO");
+        }
+    }
+}                     
