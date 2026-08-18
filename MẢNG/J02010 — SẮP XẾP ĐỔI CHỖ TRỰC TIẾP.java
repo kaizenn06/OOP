53d374db730775b7000f27e90c22e320e2c27072
Kaizen <haibui1362006hd@gmail.com>
2026-08-18 08:29:22 +0700

    Create J02010 — SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.java

---
+++ MẢNG/J02010 — SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class b24dccn186 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0 ; i < n ; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0 ; i < n - 1 ; i++) {
+            for (int j = i + 1 ; j < n ; j++) {
+                if (a[i] > a[j]) {
+                    int t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+            }
+            System.out.printf("Buoc %d: ", i + 1);
+            for (int j = 0 ; j < n ; j++) {
+                System.out.print(a[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
