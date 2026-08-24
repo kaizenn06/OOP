ad1af03d4ece9d63d86c4b2bc5cdcb397f705819
Kaizen <haibui1362006hd@gmail.com>
2026-08-24 10:10:03 +0700

    Create HELLOFILE — HELLO FILE.java

---
+++ LẬP TRÌNH JAVA CƠ BẢN/HELLOFILE — HELLO FILE.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class b24dccn186 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("Hello.txt"));
+        while(sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
