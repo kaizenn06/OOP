5bd7affc54da948fd994e40ece157d7e93d03f35
Kaizen <haibui1362006hd@gmail.com>
2026-08-27 14:53:02 +0700

    Create J07001 — ĐỌC FILE VĂN BẢN.java

---
+++ VÀO RA FILE/J07001 — ĐỌC FILE VĂN BẢN.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class b24dccn186 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
+}
