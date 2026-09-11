32dae3f407d7b8131a423fb4276d0c17d29d50ba
Kaizen <haibui1362006hd@gmail.com>
2026-09-11 09:47:00 +0700

    Add files via upload
    PHÂN SỐ

---
+++ KHAI BÁO LỚP VÀ ĐỐI TƯỢNG/J04003/src/Main.java
@@ -0,0 +1,12 @@
+package J04003;
+
+import java.util.*;
+import java.text.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PhanSo p = new PhanSo(sc.nextLong(), sc.nextLong());
+        System.out.println(p);
+    }
+}
